604250c631b4c6d817e5f613192062d30d470f38
Daniel Benavides <danibenavi1907@gmail.com>
2025-02-17 22:13:12 -0600

    finish crud webServer

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
+  routes: Router;
   port: number;
   public_path?: string;
 }
@@ -11,19 +12,27 @@ export class Server {
 
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, public_path = 'public' } = options;
+    const { port, routes, public_path = 'public' } = options;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
   async start() {
     // middleware
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
 
     // public
-
     this.app.use(express.static(this.publicPath));
 
+    // * Routes
+
+    this.app.use(this.routes);
+
+    // * SPA
     this.app.get('/*', (req, res) => {
       console.log(req.url);
 
